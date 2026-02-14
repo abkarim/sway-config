11f3d4a2914debfc8ad34cda13c74c0d9b19132d
Karim <akskarim17@gmail.com>
2026-02-14 08:02:05 +0600

    added blutooth

---
+++ etc/i3blocks/bluetooth.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Handle Click: Open the Manager
+if [[ "$BLOCK_BUTTON" -eq 1 ]]; then
+    swaymsg exec "foot -a bt_float bluetui"
+fi
+
+# Get Status
+STATE=$(bluetoothctl show | grep "Powered: yes" | wc -l)
+
+if [ "$STATE" -eq 0 ]; then
+    echo "Off"
+else
+    # Check for connected devices
+    DEVICE=$(bluetoothctl info | grep "Name:" | cut -d ' ' -f 2-)
+    if [ -z "$DEVICE" ]; then
+        echo "On"
+    else
+        echo "$DEVICE"
+    fi
+fi
