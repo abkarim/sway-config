3dd209aeadf4674af96b6daf6d9c4a4018f56255
Karim <akskarim17@gmail.com>
2026-02-14 10:05:20 +0600

    added wiremix link

--- etc/i3blocks/volume.sh
+++ etc/i3blocks/volume.sh
@@ -9,3 +9,8 @@ volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%.0f%%\n", $2 * 1
 muted=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $3}')
 
 echo "$volume $muted"
+
+# If left-clicked, open wiremix in a new terminal window
+if [[ "$BLOCK_BUTTON" -eq 1 ]]; then
+    swaymsg exec "foot -a wiremix_float wiremix"
+fi
