29996b69b5a8fdb75bd5b26972b14385d27a2839
Karim <akskarim17@gmail.com>
2026-02-14 09:49:18 +0600

    fix status

--- etc/i3blocks/bluetooth.sh
+++ etc/i3blocks/bluetooth.sh
@@ -6,13 +6,13 @@ if [[ "$BLOCK_BUTTON" -eq 1 ]]; then
 fi
 
 # Get Status
-STATE=$(bluetoothctl show | grep "Powered: yes" | wc -l)
+STATE=$(echo "show" | bluetoothctl | grep "Powered: yes" | wc -l)
 
 if [ "$STATE" -eq 0 ]; then
     echo "Off"
 else
     # Check for connected devices
-    DEVICE=$(bluetoothctl info | grep "Name:" | cut -d ' ' -f 2-)
+    DEVICE=$(echo "info" | bluetoothctl | grep "Name:" | cut -d ' ' -f 2-)
     if [ -z "$DEVICE" ]; then
         echo "On"
     else
