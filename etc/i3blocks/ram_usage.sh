1ca22278801f8edd65316139a04c909494531a9d
Karim <akskarim17@gmail.com>
2026-04-27 01:06:20 +0600

    added colors

--- etc/i3blocks/ram_usage.sh
+++ etc/i3blocks/ram_usage.sh
@@ -3,3 +3,11 @@ MEM_TOTAL=$(free -m | awk 'NR==2{print $2}')
     # Use bc for floating-point calculation
     MEM_USAGE_PERCENT=$(bc <<< "scale=2; 100 * $MEM_USED / $MEM_TOTAL")
     echo "$MEM_USAGE_PERCENT%"
+    echo "$MEM_USAGE_PERCENT%"
+    if [ "$MEM_USAGE_PERCENT" -gt 85 ]; then
+    echo "#ff5555" # Red (Heavy load)
+elif [ "$MEM_USAGE_PERCENT" -gt 60 ]; then
+    echo "#f1fa8c" # Yellow (Medium load)
+else
+    echo "#50fa7b" # Green (Healthy)
+fi
